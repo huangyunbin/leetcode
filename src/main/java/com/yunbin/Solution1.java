56b3798fce896a48089a1d34f90b91dab09db336
cloud.huang <cloud.huang@vipshop.com>
2018-01-11 16:25:15 +0800

    题目1 第一次提交

---
+++ src/main/java/com/yunbin/Solution1.java
@@ -0,0 +1,40 @@
+package com.yunbin;
+
+import java.util.Arrays;
+
+/**
+ * Created by cloud.huang on 18/1/11.
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class Solution1 {
+
+
+    public static void main(String[] args) {
+        int[] result = new Solution1().twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new RuntimeException("no answer");
+
+    }
+}
