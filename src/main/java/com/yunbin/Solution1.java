6703eb00fba36d5bd09a0244d1b0ec26ac295535
cloud.huang <cloud.huang@vipshop.com>
2018-01-11 16:29:31 +0800

    题目1 第二次提交，这次是正确了

--- src/main/java/com/yunbin/Solution1.java
+++ src/main/java/com/yunbin/Solution1.java
@@ -18,14 +18,14 @@ public class Solution1 {
 
 
     public static void main(String[] args) {
-        int[] result = new Solution1().twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] result = new Solution1().twoSum(new int[]{3, 2, 4}, 6);
         System.out.println(Arrays.toString(result));
     }
 
 
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - 1; j++) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
                 if (i == j) {
                     continue;
                 }
