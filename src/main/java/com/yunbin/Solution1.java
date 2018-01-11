e1172e64fc9fb51f08e4940b406f07f65fc1b616
cloud.huang <cloud.huang@vipshop.com>
2018-01-11 16:33:00 +0800

    题目1 第三次提交，这个才是标准答案。。

--- src/main/java/com/yunbin/Solution1.java
+++ src/main/java/com/yunbin/Solution1.java
@@ -25,10 +25,7 @@ public class Solution1 {
 
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i == j) {
-                    continue;
-                }
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
