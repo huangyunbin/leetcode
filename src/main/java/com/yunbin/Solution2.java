abd40d8a32fe3f9dd91a7ad4f0201b4837f1616a
cloud.huang <cloud.huang@vipshop.com>
2018-01-29 10:12:33 +0800

    题目2 优化进位逻辑

--- src/main/java/com/yunbin/Solution2.java
+++ src/main/java/com/yunbin/Solution2.java
@@ -10,13 +10,16 @@ public class Solution2 {
         ListNode result = new ListNode(0);
         ListNode tmp = result;
         int more = 0;
-        while (l1 != null || l2 != null || more > 0) {
+        while (l1 != null || l2 != null) {
             int val = ((l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + more) % 10;
             more = ((l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + more) / 10;
             tmp.next = new ListNode(val);
             tmp = tmp.next;
             l1 = l1 == null || l1.next == null ? null : l1.next;
             l2 = l2 == null || l2.next == null ? null : l2.next;
+        }
+        if (more > 0) {
+            tmp.next = new ListNode(more);
         }
 
         return result.next;
