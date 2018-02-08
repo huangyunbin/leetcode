e5318807a78851ceffb43dbc52b00ac7be402664
cloud.huang <cloud.huang@vipshop.com>
2018-02-08 11:14:48 +0800

    题目4 优化了下，不用合并再排序，发现还是83ms，没怎么变啊，看来是自己的代码太烂了。。

--- src/main/java/com/yunbin/Solution4.java
+++ src/main/java/com/yunbin/Solution4.java
@@ -1,7 +1,5 @@
 package com.yunbin;
 
-import java.util.Arrays;
-
 /**
  * Created by cloud.huang on 18/2/7.
  * There are two sorted arrays nums1 and nums2 of size m and n respectively.
@@ -22,21 +20,45 @@ import java.util.Arrays;
 public class Solution4 {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] array = concat(nums1, nums2);
-        Arrays.sort(array);
-        if (array.length == 1) {
-            return array[0];
-        }
+        int[] array = merge(nums1, nums2);
         if (array.length % 2 == 1) {
-            return array[array.length / 2 ];
+            return array[array.length / 2];
         } else {
-            return (array[array.length / 2-1] + array[array.length / 2 ]) / 2.0;
+            return (array[array.length / 2 - 1] + array[array.length / 2]) / 2.0;
         }
     }
 
-    public static int[] concat(int[] first, int[] second) {
-        int[] result = Arrays.copyOf(first, first.length + second.length);
-        System.arraycopy(second, 0, result, first.length, second.length);
-        return result;
+    private int[] merge(int[] nums1, int[] nums2) {
+        if (nums1.length == 0) {
+            return nums2;
+        }
+        if (nums2.length == 0) {
+            return nums1;
+        }
+        int[] array = new int[nums1.length + nums2.length];
+        for (int i = 0, j = 0, k = 0; k < nums1.length + nums2.length; k++) {
+            if (i == nums1.length) {
+                array[k] = nums2[j];
+                j++;
+                continue;
+            }
+
+            if (j == nums2.length) {
+                array[k] = nums1[i];
+                i++;
+                continue;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                array[k] = nums1[i];
+                i++;
+            } else {
+                array[k] = nums2[j];
+                j++;
+            }
+        }
+        return array;
     }
+
+
 }
--- src/test/java/com/yunbin/Solution4Test.java
+++ src/test/java/com/yunbin/Solution4Test.java
@@ -5,7 +5,7 @@ package com.yunbin;
  */
 public class Solution4Test {
     public static void main(String[] args) {
-        double result = new Solution4().findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
+        double result = new Solution4().findMedianSortedArrays(new int[]{101}, new int[]{100});
         System.out.println(result);
     }
 }
