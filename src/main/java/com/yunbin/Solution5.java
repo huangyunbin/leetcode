b3556a2698ea87c835430611fbe6c06ffa2860e9
cloud.huang <cloud.huang@vipshop.com>
2018-05-02 10:00:44 +0800

    题目5 尝试提交

---
+++ src/main/java/com/yunbin/Solution5.java
@@ -0,0 +1,72 @@
+package com.yunbin;
+
+/**
+ * Created by cloud.huang on 18/2/22.
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum
+ * length of s is 1000.
+ * <p>
+ * Example:
+ * <p>
+ * Input: "babad"
+ * <p>
+ * Output: "bab"
+ * <p>
+ * Note: "aba" is also a valid answer.
+ * <p>
+ * <p>
+ * Example:
+ * <p>
+ * Input: "cbbd"
+ * <p>
+ * Output: "bb"
+ */
+public class Solution5 {
+    
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        if (s.length() <= 1) {
+            return s;
+        }
+        char[] array = s.toCharArray();
+        int target[] = new int[]{0, 0};
+        for (int i = 0; i < length; i++) {
+            int tmp[] = getPos(i, array);
+            if (tmp[1] - tmp[0] > target[1] - target[0]) {
+                target = tmp;
+            }
+        }
+        
+        
+        return s.substring(target[0], target[1] + 1);
+    }
+    
+    private int[] getPos(int i, char[] array) {
+        int start = i;
+        int end = array.length - 1;
+        
+        for (; start < array.length && end > i; ) {
+            if (array[start] == array[end]) {
+                if (end == start + 2) {
+                    return new int[]{start, end};
+                }
+                if (end == start + 1) {
+                    if (start == i) {
+                        return new int[]{start, start + 1};
+                    } else {
+                        return new int[]{i, i + 1};
+                    }
+                }
+                
+                start++;
+                
+            } else {
+                end--;
+            }
+        }
+        
+        
+        return new int[]{start, end};
+    }
+    
+    
+}
---
+++ src/test/java/com/yunbin/Solution5Test.java
@@ -0,0 +1,11 @@
+package com.yunbin;
+
+/**
+ * Created by cloud.huang on 18/2/22.
+ */
+public class Solution5Test {
+    public static void main(String[] args) {
+        System.out.println(new Solution5().longestPalindrome("babad"));
+    }
+
+}
