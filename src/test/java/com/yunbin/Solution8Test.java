71d89b1ad4f3eb31490837d57eab57944e968d29
cloud.huang <cloud.huang@vipshop.com>
2018-09-26 09:48:18 +0800

    题目8

---
+++ src/test/java/com/yunbin/Solution8Test.java
@@ -0,0 +1,11 @@
+package com.yunbin;
+
+/**
+ * Created by cloud.huang on 18/9/26.
+ */
+public class Solution8Test {
+    public static void main(String[] args) {
+        int result = new Solution8().myAtoi("-91283472332");
+        System.out.println(result);
+    }
+}
